b8fdb376961baae5d559c4fd4ecd730e9ac16ece
Chris Hennes <chennes@gmail.com>
2023-08-24 09:05:14 -0500

    Start adding GUI tests for QuantitySpinBox

--- tests/src/Gui/QuantitySpinBox.cpp
+++ tests/src/Gui/QuantitySpinBox.cpp
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
 #include <QDebug>
+#include <QLayout>
+#include <QMainWindow>
 #include <QTest>
 
 #include <App/Application.h>
@@ -15,6 +17,8 @@ class testQuantitySpinBox: public QObject
 
 public:
     testQuantitySpinBox()
+        : qsb(nullptr)
+        , mainWindow(nullptr)
     {
         if (App::Application::GetARGC() == 0) {
             constexpr int argc = 1;
@@ -22,7 +26,24 @@ public:
             App::Application::Config()["ExeName"] = "FreeCAD";
             App::Application::init(argc, argv.data());
         }
-        qsb = std::make_unique<Gui::QuantitySpinBox>();
+        auto topLevel = qApp->topLevelWindows();
+
+        for (const auto window : topLevel) {
+            if (auto mw = qobject_cast<QMainWindow*>(window)) {
+                mainWindow = mw;
+                break;
+            }
+        }
+        if (!mainWindow) {
+            mainWindow = new QMainWindow();
+        }
+        qsb = new Gui::QuantitySpinBox;
+        if (!mainWindow->layout()) {
+            mainWindow->setLayout(new QVBoxLayout);
+        }
+        mainWindow->layout()->addWidget(qsb);
+        qsb->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        mainWindow->show();
     }
 
 private Q_SLOTS:
@@ -51,8 +72,32 @@ private Q_SLOTS:
         QCOMPARE(result, Base::Quantity(0.1, QLatin1String("mm")));
     }
 
+    void test_DefaultUnitLost()// NOLINT
+    {
+        // Arrange
+        auto foundWindow = QTest::qWaitForWindowActive(mainWindow);
+        QTEST_ASSERT(foundWindow);
+        qsb->activateWindow();
+        qsb->clear();
+        QTest::keyClicks(qsb, "1um");
+        QTest::keyClick(qsb, Qt::Key_Enter);
+        QTEST_ASSERT(qsb->value() == Base::Quantity(1, QLatin1String("um")));
+        QTest::qWait(5000);
+
+        // Act
+        QTest::mouseClick(qsb, Qt::MouseButton::LeftButton);
+        QTest::keyClicks(qsb, "3");
+        QTest::keyClick(qsb, Qt::Key_Enter);
+        qsb->evaluateExpression();
+        QTest::qWait(5000);
+
+        // Assert
+        QCOMPARE(qsb->value(), Base::Quantity(3, QLatin1String("um")));
+    }
+
 private:
-    std::unique_ptr<Gui::QuantitySpinBox> qsb;
+    Gui::QuantitySpinBox* qsb;
+    QMainWindow* mainWindow;
 };
 
 // NOLINTEND(readability-magic-numbers)
