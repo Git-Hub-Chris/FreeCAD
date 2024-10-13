acb42fd42a498a957172586ecab052dffc8014ac
Kacper Donat <kadet1090@gmail.com>
2024-10-13 13:33:18 +0200

    PartDesign: Add transparent previews
    ; Conflicts:
    ;	src/Mod/PartDesign/App/Feature.h
    ;	src/Mod/PartDesign/App/FeatureExtrude.cpp

--- src/Mod/PartDesign/App/FeatureAddSub.cpp
+++ src/Mod/PartDesign/App/FeatureAddSub.cpp
@@ -102,15 +102,21 @@ TopoShape FeatureAddSub::refineShapeIfActive(const TopoShape& oldShape, const Re
     return oldShape;
 }
 
-void FeatureAddSub::getAddSubShape(Part::TopoShape &addShape, Part::TopoShape &subShape)
+void FeatureAddSub::getAddSubShape(Part::TopoShape& addShape, Part::TopoShape& subShape)
 {
-    if (addSubType == Additive)
+    if (addSubType == Additive) {
         addShape = AddSubShape.getShape();
-    else if (addSubType == Subtractive)
+    }
+    else if (addSubType == Subtractive) {
         subShape = AddSubShape.getShape();
+    }
+}
+void FeatureAddSub::updatePreviewShape()
+{
+    PreviewShape.setValue(AddSubShape.getShape());
 }
 
-}
+}  // namespace PartDesign
 
 namespace App {
 /// @cond DOXERR
